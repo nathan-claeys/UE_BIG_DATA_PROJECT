2c3afdcd6d5432e8396e9d8a5ebecda8a7344c06
clementm <clement.monaco@imt-atlantique.net>
2025-03-23 13:02:19 +0100

    Batch affluence horaire bus

--- src/naolib_producer.py
+++ src/naolib_producer.py
@@ -134,6 +134,71 @@ def send_bus_airport():
     producer.flush()
     print(f"Sent {records} records.")
 
+def get_lines_from_stop(stop_name):
+    lines = []
+    for stop in get_stops_information():
+        if stop["codeLieu"] == stop_name:
+            for line in stop["ligne"]:
+                lines.append(line["numLigne"])
+    return lines
+
+def send_bus_affluence(stop_name):
+    lines = get_lines_from_stop(stop_name[:4])
+
+    print(lines)
+
+    producer = KafkaProducer(
+        bootstrap_servers=kafka_config["bootstrap_servers"],
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    )
+
+    print("Starting to collect bus affluence data...")
+
+    records = 0
+    
+    for line in lines:
+        url = f"https://open.tan.fr/ewp/horairesarret.json/{stop_name}/{line}/1"
+
+        print(url)
+        print("line : ")
+        print(line)
+
+        try:
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                data = response.json()
+                print("data :")
+                print(data)
+
+                num_ligne = line
+                stop = stop_name
+
+                print("records :")
+                print(records)
+
+                # Process each schedule
+                for horaire in data.get("horaires", []):
+                    heure = horaire.get("heure")
+                    for passage in horaire.get("passages", []):
+                        message = {
+                            "numLigne": num_ligne,
+                            "stop": stop,
+                            "heure": heure,
+                            "passage": passage
+                        }
+                        producer.send("bus_affluence_horaire", value=message)
+                        records += 1
+
+            else:
+                print(f"Failed to fetch data: {response.status_code}")
+        
+        except Exception as e:
+            print(f"Error fetching data for {stop_name}, line {line}: {e}")
+
+    producer.flush()
+    print(f"Sent {records} records.")
+
 
 def run_periodic(interval_sec, stop_event, task, task_args=()):
     while not stop_event.is_set():
@@ -155,6 +220,10 @@ def main():
         kafka_config["bootstrap_servers"],
         "bike_stations",
     )
+    create_topic_if_not_exists(
+        kafka_config["bootstrap_servers"],
+        "bus_affluence_horaire"
+    )
 
     # Non periodic task
     send_bus_airport()
@@ -163,6 +232,8 @@ def main():
     TEST_RADIUS = 10
 
     stop_event = threading.Event()
+
+    send_bus_affluence("CRQU4")
 
     # Create the periodic thread for bus position data
     bus_thread = threading.Thread(
